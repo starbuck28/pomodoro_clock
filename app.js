b433fc37d1fff3f1f10ee2b742c1402e7b21c08f
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2016-09-26 14:54:11 -0400

    repository of existing project initialized

---
+++ app.js
@@ -0,0 +1,63 @@
+// JavaScript Document
+
+//Initial break length in minutes
+var breakLength = 5;
+//Initial session length in minutes
+var sessionLength = 25;
+
+//Adds 1 minute to length to max of 60 min
+function addParameter(time) {
+  if (time === 60) {
+    return time;
+  }
+  return time = time + 1;
+}
+
+//Subtracts 1 minute from length to min of 1 min
+function subtractParameter(time) {
+  if(time === 1) {
+    return time;
+  }
+  return time = time - 1;
+}
+
+
+
+//When break subtract button clicked
+$("#break-subtract").click(function() {
+    //Break length decreases by 1 until min of 1
+    subtractParameter(breakLength);
+  //Break length updated on screen
+    $("#break-length").html('<p id="break-length" class="length">' + breakLength + '</p>');
+  console.log(breakLength);
+  //Timer is set to mirror session length
+  console.log("subtract");
+});
+
+//When break add button clicked
+$("#break-add").click(function() {
+  //Break length increases by 1 to max of 60
+    addParameter(breakLength);
+  //Break length updated on screen
+    $("#break-length").html('<p id="break-length" class="length">' + breakLength + '</p>');
+  console.log(breakLength);
+  console.log("add");
+});
+
+//When clock toggled
+$(".clock").click(function() {
+  //Timer starts or stops
+  
+  console.log("clock");
+});
+
+//When session subtract button clicked
+$("#session-subtract").click(function() {
+    //Break length decreases by 1 until min of 1
+    subtractParameter(sessionLength);
+  //Break length updated on screen
+    $("#session-length").html('<p id="session-length" class="length">' + sessionLength + '</p>');
+  console.log(sessionLength);
+  //Timer is set to mirror session length
+  console.log("subtract");
+});
---
+++ tests/tests.js
@@ -0,0 +1,5 @@
+//Test Suite for Pomodoro Clock
+
+QUnit.test("breakLength exists", function(assert) {
+	assert.ok(breakLength === 5, "breakLength is initially 5");
+	});
